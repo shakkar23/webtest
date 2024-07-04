d0ab62ed9f88a46d8be7eb66d1dead52715914d9
shakkar23 <shakkar23@gmail.com>
2024-07-04 14:15:08 -0700

    another push

--- wasm_user.js
+++ wasm_user.js
@@ -2,7 +2,7 @@
 
 
 var result = Module.onRuntimeInitialized = () => {
-    let buf = Int32Array(1);
+    let buf = new Int32Array(1);
     const result = Module._add(buf.data);
     console.log(buf[0]);
 };
